dc2adf2788f58f062864a8c2a1d5bebee1da0dc8
AxelFulop <axelfulop@hotmail.com>
2020-04-28 10:23:31 -0300

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var app = express();
 //Respond with "hello world" for requests that hit our root "/"
 app.get('/', function (req, res) {
     res.send('hello world,testing build');
+    res.send('just testing the cicd pipeline');
 });
 //listen to port 3000 by default
 app.listen(process.env.PORT || 3000);
