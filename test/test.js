9c2c7b3e1084e2073a3635086c6cac0409001f8c
axel fulop <axelfulop@hotmail.com>
2020-05-04 16:25:26 -0300

    dleee

--- test/test.js
+++ test/test.js
@@ -3,10 +3,6 @@ var app = require('../index.js');
 describe('GET /', function () {
     it('respond with hello world', function (done) {
         //navigate to root and check the the response is "hello world"
-<<<<<<< HEAD
         request(app).get('/').expect('hello world,cicd on', done);
-=======
-        request(app).get('/').expect('hello world,testing build ^^', done);
->>>>>>> f00057f6fb778a282bf5d376adc0a387806c0c8d
     });
 });
