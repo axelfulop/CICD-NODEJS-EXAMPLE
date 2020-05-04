956570aff0fd11d4bb47c2fb2a26a0a0c23b8fc6
axel fulop <axelfulop@hotmail.com>
2020-05-04 16:16:33 -0300

    build change

--- index.js
+++ index.js
@@ -4,11 +4,7 @@ var express = require('express');
 var app = express();
 //Respond with "hello world" for requests that hit our root "/"
 app.get('/', function (req, res) {
-<<<<<<< HEAD
     res.send('hello world,cicd on');
-=======
-    res.send('hello world,testing build ^^');
->>>>>>> f00057f6fb778a282bf5d376adc0a387806c0c8d
 });
 //listen to port 3000 by default
 app.listen(process.env.PORT || 3000);
